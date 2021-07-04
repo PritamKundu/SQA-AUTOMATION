84bdf9684e54cf86bbfad4ae90cd341f20493696
Pritam <pritamkundu.pk5@gmail.com>
2021-07-05 00:03:22 +0600

    navigate the new page

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -5,13 +5,23 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub\
 	 WebDriverManager.firefoxdriver().setup();
      WebDriver driver = new FirefoxDriver();
      driver.get("http://pritamkundu.xyz");
      
-		
+     //Thread.sleep(5000);
+     
+     //driver.close();
+     
+     System.out.println("Title of the page :" +driver.getTitle());
+     System.out.println("Url of the page :"+driver.getCurrentUrl());
+     System.out.println("Source code of the page "+driver.getPageSource());
+     
+     
+     
 	}
+
 
 }
