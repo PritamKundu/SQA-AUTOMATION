98e01c84dac93a596477047f951a8e34aa8fec26
Pritam <pritamkundu.pk5@gmail.com>
2021-09-08 23:19:41 +0600

    werrrr

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -10,7 +10,7 @@ public class Test {
 		// TODO Auto-generated method stub\
 	 WebDriverManager.firefoxdriver().setup();
      WebDriver driver = new FirefoxDriver();
-     driver.get("https://nexchar.com/signoin1");
+     driver.get("https://nexchar.com/signoin12");
      
 //     //Thread.sleep(5000);
 //     
