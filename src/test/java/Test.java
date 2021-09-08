fd6b397af5ef9735ebca2ab6f0f277029063a283
Pritam <pritamkundu.pk5@gmail.com>
2021-09-08 15:17:33 +0600

    first commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -10,7 +10,7 @@ public class Test {
 		// TODO Auto-generated method stub\
 	 WebDriverManager.firefoxdriver().setup();
      WebDriver driver = new FirefoxDriver();
-     driver.get("https://nexchar.com/signin");
+     driver.get("https://nexchar.com/signout");
      
 //     //Thread.sleep(5000);
 //     
